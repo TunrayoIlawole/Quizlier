9f41e6ff54f08a5ab4a30f750ac4fba201a0e3e8
Motunrayo Ilawole <motunrayo.ilawole@flutterwavego.com>
2024-03-06 14:08:50 +0100

    initial project setup

---
+++ core/src/test/java/com/quizlier/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizlier.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
