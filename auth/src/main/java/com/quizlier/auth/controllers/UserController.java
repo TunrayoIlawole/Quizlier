fc98bec83ca05948b6e2fa994e68ca632cca022d
Motunrayo Ilawole <motunrayo.ilawole@flutterwavego.com>
2024-04-25 20:18:53 +0100

    remove bearer token endpoint

--- auth/src/main/java/com/quizlier/auth/controllers/UserController.java
+++ auth/src/main/java/com/quizlier/auth/controllers/UserController.java
@@ -67,7 +67,6 @@ public class UserController {
 		return userService.signInUser(userLoginRequest);
 	}
 	
-	@PostMapping("/generateToken")
 	public String authenticateAndGetToken(@RequestBody AuthRequest authRequest) {
 		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
 		
