e850b3ddec87b8979847f4c7047b0f5771ca3df3
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-07-07 18:03:58 +0100

    add entity scan config

--- auth/src/main/java/com/quizlier/auth/AuthApplication.java
+++ auth/src/main/java/com/quizlier/auth/AuthApplication.java
@@ -3,8 +3,13 @@ package com.quizlier.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.quizlier.auth.repository")
+@EntityScan(basePackages = {"com.quizlier.common.entity"})
+//@ComponentScan(basePackages = "com.quizlier.auth")
 public class AuthApplication {
 
 	public static void main(String[] args) {
--- core/src/main/java/com/quizlier/core/CoreApplication.java
+++ core/src/main/java/com/quizlier/core/CoreApplication.java
@@ -3,8 +3,11 @@ package com.quizlier.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.quizlier.common.entity"})
+@EnableJpaRepositories(basePackages = "com.quizlier.core.repository")
 public class CoreApplication {
 
 	public static void main(String[] args) {
