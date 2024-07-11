24154e2984addf6ce88abdaa0935c2b7347941f3
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-07-11 18:15:00 +0100

    add discovery config

--- auth/src/main/java/com/quizlier/auth/AuthApplication.java
+++ auth/src/main/java/com/quizlier/auth/AuthApplication.java
@@ -3,12 +3,14 @@ package com.quizlier.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.quizlier.auth.repository")
 @EntityScan(basePackages = {"com.quizlier.common.entity"})
+@EnableJpaRepositories(basePackages = "com.quizlier.auth.repository")
+@EnableDiscoveryClient
 //@ComponentScan(basePackages = "com.quizlier.auth")
 public class AuthApplication {
 
