d6820d1247b12023e07e971225b89e5bb305e618
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-01 10:29:07 +0100

    add maximum entity exception

---
+++ core/src/main/java/com/quizlier/core/exceptions/MaximumEntityException.java
@@ -0,0 +1,8 @@
+package com.quizlier.core.exceptions;
+
+public class MaximumEntityException extends Exception {
+
+    public MaximumEntityException(String message) {
+        super(message);
+    }
+}
